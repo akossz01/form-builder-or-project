5e50d937d9079c9d29b129377e236c03702e0109
Fazakas Nimród <kriszterik1@gmail.com>
2024-04-09 15:48:16 +0300

    Update routes and statistics component to display a chart of votes v1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
 import { StatisticsComponent } from './statistics/statistics.component';
 
-const routes: Routes = [
+export const routes: Routes = [
     {
         path: 'statistics', // localhost:4200/statistics
         component: StatisticsComponent
     }
 ];
-
-//export const routes: Routes = [];
