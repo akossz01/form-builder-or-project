0120810ba8c63bb97222ef2e75bfc64794a1c582
Akos Szabo <akossz12@gmail.com>
2024-04-10 17:53:53 +0300

    completed earlier disable form

--- src/app/components/dynamic-form/dynamic-form.component.ts
+++ src/app/components/dynamic-form/dynamic-form.component.ts
@@ -7,39 +7,59 @@ import { ActivatedRoute } from '@angular/router';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './dynamic-form.component.html',
-  styleUrl: './dynamic-form.component.css'
+  styleUrl: './dynamic-form.component.css',
 })
 export class DynamicFormComponent {
   formDetails: any;
   selectedOptions: string[] = [];
   errorNoSelection: boolean = false;
   submitted: boolean = false;
+  completedEarlier: boolean = false;
 
   forms: any[] = [
     {
       id: 'abc123',
       title: 'Melyek a kedvenc ételeid?',
       desc: 'Kérlek válaszd ki a kedvenc ételeidet!',
-      options: ['Pizza', 'Hamburger', 'Sushi', 'Saláta', 'Pasta']
+      options: ['Pizza', 'Hamburger', 'Sushi', 'Saláta', 'Pasta'],
     },
     {
       id: 'def456',
       title: 'Form 2',
       desc: 'Description for Form 2',
-      options: ['Option A', 'Option B', 'Option C']
+      options: ['Option A', 'Option B', 'Option C'],
     },
   ];
 
   formId: string | any;
 
-  constructor(private route: ActivatedRoute) {
-  }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
+    this.route.paramMap.subscribe((params) => {
       this.formId = params.get('formId');
       this.formDetails = this.getFormData(this.formId);
     });
+
+    // Fetcheles utan megnezzuk ha az adott hasznalo kitoltotte mar a formot
+    // pl. /form/check_form_completed -> header: token, body: form_id
+    /* fetch('http://141.147.42.101:3001/form/check_form_completed', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        token: 'your_token_here',
+      },
+      body: JSON.stringify({ form_id: this.formId }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        this.completedEarlier = data.completed;
+      })
+      .catch((error) => {
+        console.error('Error checking form completion:', error);
+      }); */
+
+    // this.completedEarlier = true;
   }
 
   onCheckboxChange(option: string, event: any) {
@@ -57,7 +77,7 @@ export class DynamicFormComponent {
   }
 
   getFormData(formId: string) {
-    const formData = this.forms.find(form => form.id === formId);
+    const formData = this.forms.find((form) => form.id === formId);
     return formData;
   }
 
@@ -69,7 +89,12 @@ export class DynamicFormComponent {
       this.errorNoSelection = this.selectedOptions.length === 0;
     } else {
       this.submitted = true;
-      console.log('Form submitted with options: ' + this.selectedOptions.join(', ') + ' for form: ' + this.formId);
+      console.log(
+        'Form submitted with options: ' +
+          this.selectedOptions.join(', ') +
+          ' for form: ' +
+          this.formId
+      );
     }
   }
 }
